f878db221542f94f25ff88ae58012a1987209526
e1eng <inisialt12@gmail.com>
2025-09-21 17:39:41 +0700

    Add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   reactStrictMode: true,
   images: {
+    unoptimized: true,
+
     domains: ['lh3.googleusercontent.com'], 
   },
 }
+
 module.exports = nextConfig
